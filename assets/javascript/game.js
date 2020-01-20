8d61b4ff94f41e4b0bfebb53daaab93e161399d0
Brian Allen <brianallen2401@gmail.com>
2020-01-20 11:10:56 -0700

    added comments and variables to hold button values and win/loss values

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//Global variables go here!!
+//variables for each button to hold the randomly assigned value
+var magnesiumPyrite;
+var amethyst;
+var dragonGlass;
+var heartstone;
+//variables to hold win/loss values
+var wins = 0;
+var losses = 0;
