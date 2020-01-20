923d1ac877711cc4e4ef73acc3e83fef195af2f2
Brian Allen <brianallen2401@gmail.com>
2020-01-20 14:08:37 -0700

    added reset function. Not called yet, but should reset game state for a new game appropriately.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,12 +12,23 @@ $(document).ready(function () {
     var losses = 0;
     //variable to hold potion progress value
     var potionProgress = 0;
-
+    //logic to push progress, wins and losses to the page
     $("#potion-progress").text(potionProgress);
     $("#wins").text("Wins: " + wins);
     $("#losses").text("Losses: " + losses);
 });
 
+//function to restart the game upon win/loss
+function reset(){
+    potionValue = Math.floor(Math.random() * 101 + 19);
+    $("#potion-score").text("Target Potion Value: " + potionValue);
+    magnesiumPyrite = Math.floor(Math.random() * 11 + 1);
+    amethyst = Math.floor(Math.random() * 11 + 1);
+    dragonGlass = Math.floor(Math.random() * 11 + 1);
+    heartstone = Math.floor(Math.random() * 11 + 1);
+    potionProgress = 0;
+    $("#potion-progress").text(potionProgress);
+}
 
 
 
